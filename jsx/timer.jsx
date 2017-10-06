9a2ccdcc4aa93ac51fcdeedc4caba0f7875eb6e0
Jayant Sarkar <codejayant@gmail.com>
2017-10-06 11:37:28 -0700

    running timer code

---
+++ jsx/timer.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+
+class Timer extends React.Component {
+  render() {
+    if (this.props.timeLeft == 0) {
+      document.getElementById('end-of-time').play()
+    }
+    if (this.props.timeLeft == null || this.props.timeLeft == 0)
+      return <div/>
+    return <h1>Time left: {this.props.timeLeft}</h1>
+  }
+}
+
+module.exports = Timer
