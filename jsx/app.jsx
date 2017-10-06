afefc4926eee866665622b09461060d5e9a63c92
Jayant Sarkar <codejayant@gmail.com>
2017-10-06 13:16:01 -0700

    added pause and resume timer feature

--- jsx/app.jsx
+++ jsx/app.jsx
@@ -8,18 +8,31 @@ class TimerWrapper extends React.Component {
     super(props)
     this.state =  {timeLeft: null, timer: null}
     this.startTimer = this.startTimer.bind(this)
+    this.pausetime = this.pausetime.bind(this)
+    this.resumetime = this.resumetime.bind(this)
   }
   startTimer(timeLeft) {
     clearInterval(this.state.timer)
     let timer = setInterval(() => {
-      console.log('2: Inside of setInterval')
-      var timeLeft = this.state.timeLeft - 1
-      if (timeLeft == 0) clearInterval(timer)
-      this.setState({timeLeft: timeLeft})
+        console.log('2: Inside of setInterval')
+        var timeLeft = this.state.timeLeft - 1
+        if (timeLeft == 0) clearInterval(timer)
+        this.setState({timeLeft: timeLeft})
     }, 1000)
     console.log('1: After setInterval')
     return this.setState({timeLeft: timeLeft,
       timer: timer})
+  }
+  pausetime() {
+    console.log('pausing time at time : ' + this.state.timeLeft)
+    clearInterval(this.state.timer)
+    return this.setState({
+      timeLeft: this.state.timeLeft
+    })
+  }
+  resumetime()  {
+    console.log('resuming time from time : ' + this.state.timeLeft)
+    return this.startTimer(this.state.timeLeft)
   }
   render() {
     return (
@@ -29,6 +42,20 @@ class TimerWrapper extends React.Component {
           <Button time="5" startTimer={this.startTimer}/>
           <Button time="10" startTimer={this.startTimer}/>
           <Button time="15" startTimer={this.startTimer}/>
+        </div>
+        <div>
+          <button 
+            type="button" 
+            className='btn-default btn'
+            onClick={this.pausetime}>
+            PAUSE
+          </button>
+          <button 
+            type="button" 
+            className='btn-default btn'
+            onClick={this.resumetime}>
+            RESUME
+          </button>
         </div>
         <Timer timeLeft={this.state.timeLeft}/>
       <audio id="end-of-time" src="flute_c_long_01.wav" preload="auto"></audio>
